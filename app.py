cf4f0c1d918f049e616e701c788af9a2cfc4174e
Someone-Yang <stevenyoung1120@outlook.com>
2025-03-25 12:16:08 +0800

    feat: 使用Canvas绘图

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import tkinter as tk
+import canvas
+import time
 
 # 主窗口
 root = tk.Tk()
 
 root.geometry("1920x420")
 root.overrideredirect(True)
+global baseCanvas
 baseCanvas = tk.Canvas(root, width=1920, height=420, bg='white')
 baseCanvas.pack()
 
@@ -32,35 +35,63 @@ nextStation = 3
 terminus = stations[len(stations) - 1]
 
 baseCanvas.create_image(50, 60, anchor=tk.W, image=logoImage)
-baseCanvas.create_text(400, 60, text="114", font=('黑体', 20))
-baseCanvas.create_text(600, 60, text=f"开往：{terminus['name']}\nTerminus:{terminus['name_eng']}", font=('黑体', 20))
-baseCanvas.create_text(1200, 60, text=f"下一站：{stations[nextStation]['name']}\nNext Station:{stations[nextStation]['name_eng']}", font=('黑体', 20))
+baseCanvas.create_text(400, 60, text="测试线路文件", font=('黑体', 20),anchor="w")
+baseCanvas.create_text(700, 60, text=f"开往：{terminus['name']}\nTerminus:{terminus['name_eng']}", font=('黑体', 20),anchor="w")
+baseCanvas.create_text(1200, 60, text=f"下一站：{stations[nextStation]['name']}\nNext Station:{stations[nextStation]['name_eng']}", font=('黑体', 20),anchor="w")
 
+global nsashow
+nsashow = False
+global printEach
+global printArrow
+global nextStationLoc
+global nextStationArrowLoc
 printSta = 0
 printLoc = 60
 printEach = (1920 - printLoc - printLoc - 432)/((len(stations) - 1)*2)
+printArrow = (printEach - 16) / 2
+nextStationLoc = 0
+nextStationArrowLoc = 0
 for station in stations:
   print(printLoc)
   baseCanvas.create_text(printLoc, 300, text=station['name'], font=('黑体', 22), angle=45, anchor=tk.SW)
-  baseCanvas.create_text(printLoc + 20, 300, text=station['name_eng'], font=('黑体', 14), angle=45, anchor=tk.SW)
+  baseCanvas.create_text(printLoc + 24, 300, text=station['name_eng'], font=('黑体', 14), angle=45, anchor=tk.SW)
   if printSta < nextStation:
-    targerImage = greyImage
+    canvas.draw_gradient_ball(baseCanvas,printLoc,320,22,(0xFF, 0xFF, 0xFF),(0xAA, 0xAA, 0xAA),10)
   elif printSta > nextStation:
-    targerImage = greenImage
+    canvas.draw_gradient_ball(baseCanvas,printLoc,320,22,(0xFF, 0xFF, 0xFF),(0x00, 0x99, 0x00),10)
   else:
-    targerImage = redImage
-  baseCanvas.create_image(printLoc, 320, anchor=tk.CENTER, image=targerImage)
+    canvas.draw_gradient_ball(baseCanvas,printLoc,320,22,(0xFF, 0xFF, 0xFF),(0xFF, 0x00, 0x00),10)
+    nextStationLoc=printLoc
+  
   printLoc = printLoc + printEach
   # arrow
   if printSta + 1 < len(stations):
-    if printSta + 1 < nextStation:
-      targerImage = greyArrowImage
+    if printSta + 1 <= nextStation:
+      arrowColor = "#AAAAAA"
     elif printSta + 1 > nextStation:
-      targerImage = greenArrowImage
-    else:
-      targerImage = redArrowImage
-    baseCanvas.create_image(printLoc, 320, anchor=tk.CENTER, image=targerImage)
+      arrowColor = "#00FF00"
+    canvas.draw_arrow(baseCanvas,printLoc,320,arrowColor)
+    canvas.draw_arrow(baseCanvas,printLoc - printArrow,320,arrowColor)
+    canvas.draw_arrow(baseCanvas,printLoc + printArrow,320,arrowColor)
+    if printSta + 1 == nextStation:
+      nextStationArrowLoc=printLoc
   printLoc = printLoc + printEach
   printSta = printSta + 1
 
-root.mainloop()
+def shiftArrows():
+  global nsashow
+  while True:
+    root.update()
+    time.sleep(1)
+    if nsashow:
+      baseCanvas.delete("nextarr")
+      nsashow = False
+    else:
+      canvas.draw_arrow(baseCanvas,nextStationArrowLoc,320,"#CC0000","nextarr")
+      canvas.draw_arrow(baseCanvas,nextStationArrowLoc - printArrow,320,"#CC0000","nextarr")
+      canvas.draw_arrow(baseCanvas,nextStationArrowLoc + printArrow,320,"#CC0000","nextarr")
+      nsashow = True
+
+shiftArrows()
+
+  
---
+++ canvas.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import math
+
+def draw_gradient_ball(targetCanvas:tk.Canvas ,x, y, radius, start_color, end_color, steps):
+  for i in range(steps):
+    ratio = i / steps
+    r = int(start_color[0] + (end_color[0] - start_color[0]) * ratio)
+    g = int(start_color[1] + (end_color[1] - start_color[1]) * ratio)
+    b = int(start_color[2] + (end_color[2] - start_color[2]) * ratio)
+    color = f"#{r:02x}{g:02x}{b:02x}"
+    current_radius = radius * (1 - ratio * 0.5)
+    targetCanvas.create_oval(
+        x - current_radius, y - current_radius,
+        x + current_radius, y + current_radius,
+        fill=color, outline=color
+    )
+
+def draw_arrow(targetCanvas:tk.Canvas, x, y, color, tags = "normal"):
+    targetCanvas.create_polygon(
+        (x,y),(x-8,y+16),(x+8,y+16),(x+16,y),(x+8,y-16),(x-8,y-16),
+        fill=color, outline="#888888", width=2, tags=tags
+)
